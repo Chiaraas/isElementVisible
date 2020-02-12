73dd2e4cab84722dc0f80a5d9876403c17a2df55
Unknown <eaachis@students.eaaa.dk>
2020-02-12 10:34:21 +0100

    in class 12.02.2020

--- js/audio.js
+++ js/audio.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", function() { // detect scroll event
   var mySound = document.getElementById("myaudio"); // select audio id
   var lyd = document.getElementById("lyd");
   console.log("Er div #lyd synlig? " + elFllVsbl(lyd)); // test
-
+        
 // Below: start and stop audio 
   if (elFllVsbl(lyd)) { // if the #lyd div is visible then
     if (!(mySound.curentTime > 0)) {
@@ -22,8 +22,55 @@ window.addEventListener("scroll", function() { // detect scroll event
 // this function will check whether a tag is visible
 function elFllVsbl(el) {
   return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
-}
+} 
 
+
+
+window.addEventListener("scroll", function() { // detect scroll event    
+
+  var mySound = document.getElementById("myaudio2"); // select audio id
+  var music = document.getElementById("music");
+  console.log("Er div #music synlig? " + elFllVsbl(music)); // test    
+
+// Below: start and stop audio 
+  if (elFllVsbl(music)) { // if the #Music div is visible then
+    if (!(mySound.curentTime > 0)) {
+      mySound.play(); // play audio
+    }
+  } else {
+    mySound.pause();
+    mySound.currentTime = 0; // rewind the sound
+  }
+})
+
+// this function will check whether a tag is visible
+function elFllVsbl(el) {
+  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
+} 
+
+
+window.addEventListener("scroll", function() { // detect scroll event    
+
+  var myVideo = document.getElementById("my see"); // select audio id
+  var video = document.getElementById("isee");
+  console.log("Er div #music synlig? " + elFllVsbl(isee)); // test    
+
+// Below: start and stop audio 
+  if (elFllVsbl(isee)) { // if the #isee div is visible then
+    if (!(myVideo.curentTime > 0)) {
+      myVideo.play(); // play audio
+    }
+  } else {
+    myVideo.pause();
+    myVideo.currentTime = 0; // rewind the sound
+  }
+})
+
+// this function will check whether a tag is visible
+function elFllVsbl(el) {
+  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
+}     
+    
 /*
 NB: the audio / video tags will allways return true,
 solution: place the tag in a div and test for the div in stead
